6b12e446aaed47dbf66e51d42710c07a7c57ab38
sienge <enge666-86@yandex.ru>
2019-01-20 17:32:34 +0300

    Фиксирование шапки

--- PSBox/js/script.js
+++ PSBox/js/script.js
@@ -1,39 +1,39 @@
 //Прилипание шапки при прокрутке
 
-var HeaderFixed = (function() {
-
-    var docElem = document.documentElement,
-        header = document.querySelector( '.header' ),
-        didScroll = false,
-        changeHeaderOn = 300;
-
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            header.classList.add('header--min' );
-        }
-        else {
-            header.classList.remove('header--min' );
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
+// var HeaderFixed = (function() {
+//
+//     var docElem = document.documentElement,
+//         header = document.querySelector( '.header' ),
+//         didScroll = false,
+//         changeHeaderOn = 300;
+//
+//     function init() {
+//         window.addEventListener( 'scroll', function( event ) {
+//             if( !didScroll ) {
+//                 didScroll = true;
+//                 setTimeout( scrollPage, 250 );
+//             }
+//         }, false );
+//     }
+//
+//     function scrollPage() {
+//         var sy = scrollY();
+//         if ( sy >= changeHeaderOn ) {
+//             header.classList.add('header--min' );
+//         }
+//         else {
+//             header.classList.remove('header--min' );
+//         }
+//         didScroll = false;
+//     }
+//
+//     function scrollY() {
+//         return window.pageYOffset || docElem.scrollTop;
+//     }
+//
+//     init();
+//
+// })();
 
 
 //Обработка события клика на "бургер" навигации. При клике разворачивается меню
