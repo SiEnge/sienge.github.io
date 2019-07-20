157569d1df232af056b588b9056f669d777e07aa
sienge <enge666-86@yandex.ru>
2019-07-21 01:00:42 +0300

    настройка фильтра и раскладки (размер L)

--- Nikamed/js/script.js
+++ Nikamed/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("click", function(event) {
   var target = event.target;
+
+
+  //открытие/закрытие фильтра
   if (target.classList.contains("filter__btnOpen")) {
     let filter = document.querySelector(".filter");
     if (filter) {
@@ -10,4 +13,89 @@ document.addEventListener("click", function(event) {
       }
     }
   }
+
+  //выбор параметров фильтра
+  if (target.classList.contains("filter__item")) {
+    if (target.dataset.check != "true") {
+      target.dataset.check = "true";
+      let filter = document.querySelector(".filter");
+      if (filter) {
+        let filterChoice = filter.querySelector(".filter__choice");
+        let filterItemClone = target.cloneNode(true);
+        filterItemClone.classList.remove("filter__item");
+        filterItemClone.classList.add("filter__itemChoice");
+        filterChoice.appendChild(filterItemClone);
+      }
+    } else {
+      target.dataset.check = "false";
+      let filter = document.querySelector(".filter");
+      if (filter) {
+        let filterChoice = filter.querySelector(".filter__choice");
+        let filterItemsChoice = filter.querySelectorAll(".filter__itemChoice");
+        for (var i = 0; i < filterItemsChoice.length; i++) {
+          if (filterItemsChoice[i].dataset.id == target.dataset.id) {
+            filterChoice.removeChild(filterItemsChoice[i]);
+          }
+        }
+      }
+    }
+  }
+
+  //удаление выбранных параметров фильтра
+  if (target.classList.contains("filter__itemChoice")) {
+    let filter = document.querySelector(".filter");
+    if (filter) {
+      let filterChoice = filter.querySelector(".filter__choice");
+      filterChoice.removeChild(target);
+      let filterItems = filter.querySelectorAll(".filter__item");
+      for (var i = 0; i < filterItems.length; i++) {
+        if (filterItems[i].dataset.id == target.dataset.id) {
+          filterItems[i].dataset.check = "false";
+        }
+      }
+    }
+  }
+
+  //применить в фильтре
+  if (target.classList.contains("filter__btnApply")) {
+    let filter = document.querySelector(".filter");
+    filter.dataset.mode = "close";
+  }
+
+  //отмена в фильтре
+  if (target.classList.contains("filter__btnCancel")) {
+    clearFilter();
+    let filter = document.querySelector(".filter");
+    filter.dataset.mode = "close";
+  }
+
+
+  //сбросить все фильтры
+  if (target.classList.contains("filter__btnReset")) {
+    clearFilter();
+  }
+
+  //переключение раскладки
+  if (target.classList.contains("main__btnLayout")) {
+    let allFiles = document.querySelector(".main__allFiles");
+    if (allFiles) {
+      allFiles.dataset.mode = (allFiles.dataset.mode == "sizeM") ? "sizeL" : "sizeM";
+    }
+  }
+
 });
+
+function clearFilter() {
+  let filter = document.querySelector(".filter");
+  if (filter) {
+    let filterChoice = filter.querySelector(".filter__choice");
+    let filterItemsChoice = filter.querySelectorAll(".filter__itemChoice");
+    for (var i = 0; i < filterItemsChoice.length; i++) {
+      filterChoice.removeChild(filterItemsChoice[i]);
+    }
+    let filterItems = filter.querySelectorAll(".filter__item");
+    for (var i = 0; i < filterItems.length; i++) {
+      filterItems[i].dataset.check = "false";
+    }
+  }
+}
