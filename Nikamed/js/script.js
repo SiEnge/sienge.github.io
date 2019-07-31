e63dc64bb5449c92f2813189ba82cd14b7c68ca6
sienge <enge666-86@yandex.ru>
2019-08-01 01:06:39 +0300

    Настроен блок .control

--- Nikamed/js/script.js
+++ Nikamed/js/script.js
@@ -21,18 +21,21 @@ document.addEventListener("click", function(event) {
     if (target.dataset.check != "true") {
       target.dataset.check = "true";
       let filter = document.querySelector(".filter");
+      let main = document.querySelector(".main");
       if (filter) {
-        let filterChoice = filter.querySelector(".filter__choice");
+        let filterChoice = main.querySelector(".main__filterChoice");
         let filterItemClone = target.cloneNode(true);
         filterItemClone.classList.remove("filter__item");
         filterItemClone.classList.add("filter__itemChoice");
         filterChoice.appendChild(filterItemClone);
+        checkOverflow(filterChoice);
       }
     } else {
       target.dataset.check = "false";
       let filter = document.querySelector(".filter");
+      let main = document.querySelector(".main");
       if (filter) {
-        let filterChoice = filter.querySelector(".filter__choice");
+        let filterChoice = main.querySelector(".main__filterChoice");
         let filterItemsChoice = filter.querySelectorAll(".filter__itemChoice");
         for (var i = 0; i < filterItemsChoice.length; i++) {
           if (filterItemsChoice[i].dataset.id == target.dataset.id) {
@@ -46,8 +49,9 @@ document.addEventListener("click", function(event) {
   //удаление выбранных параметров фильтра
   if (target.classList.contains("filter__itemChoice")) {
     let filter = document.querySelector(".filter");
+    let main = document.querySelector(".main");
     if (filter) {
-      let filterChoice = filter.querySelector(".filter__choice");
+      let filterChoice = main.querySelector(".main__filterChoice");
       filterChoice.removeChild(target);
       let filterItems = filter.querySelectorAll(".filter__item");
       for (var i = 0; i < filterItems.length; i++) {
@@ -77,13 +81,48 @@ document.addEventListener("click", function(event) {
     clearFilter();
   }
 
-  //переключение раскладки
-  if (target.classList.contains("main__btnLayout")) {
-    let allFiles = document.querySelector(".main__allFiles");
-    if (allFiles) {
-      allFiles.dataset.mode = (allFiles.dataset.mode == "sizeM") ? "sizeL" : "sizeM";
+  // Открытие/закрытие выпадающего окна
+  let controlBtn = target.closest(".control__button--dropDown");
+  if (controlBtn) {
+    let wrap = target.closest(".control__wrap");
+    let control = document.querySelector(".control");
+    let controlWraps = control.querySelectorAll(".control__wrap");
+    for (var i = 0; i < controlWraps.length; i++) {
+      controlWraps[i].dataset.list = "hide";
     }
+    if (wrap.dataset.list == "show") {
+      wrap.dataset.list = "hide";
+    } else {
+      wrap.dataset.list = "show";
+    }
+    return;
   }
+
+  // Выбор из выпадающего списка
+  if (target.classList.contains("control__dropDownItem")) {
+    let wrap = target.closest(".control__wrap");
+    let text = target.innerHTML;
+
+    wrap.querySelector(".control__name").innerHTML = text;
+    wrap.dataset.list = "hide";
+
+    if (wrap.classList.contains("control__wrap--layout")) {
+      let allFiles = document.querySelector(".main__allFiles");
+      allFiles.dataset.mode = target.dataset.layout;
+    }
+
+
+  }
+
+  
+
+  // //переключение раскладки
+  // if (target.classList.contains("main__btnSwitchLayout")) {
+  //   let allFiles = document.querySelector(".main__allFiles");
+  //   if (allFiles) {
+  //     allFiles.dataset.mode = (allFiles.dataset.mode == "sizeM") ? "sizeL" : "sizeM";
+  //   }
+  // }
 
 });
 
@@ -100,4 +139,19 @@ function clearFilter() {
       filterItems[i].dataset.check = "false";
     }
   }
+}
+
+function checkOverflow(wrap) {
+  let widthWrap = wrap.offsetWidth;
+  let items = wrap.children;
+  let widthItems = 0;
+
+  for (var i = 0; i < items.length; i++) {
+    widthItems = widthItems + items[i].offsetWidth;
+  }
+
+  if (widthItems > widthWrap) {
+    console.log("Переполнение");
+  }
+
 }
