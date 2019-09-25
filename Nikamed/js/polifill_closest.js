0ce63fa75da7d204597492e7242edf97c499ce8c
sienge <enge666-86@yandex.ru>
2019-09-25 22:51:31 +0300

    полифилл closest для ie11

--- Nikamed/js/polifill_closest.js
+++ Nikamed/js/polifill_closest.js
@@ -1,21 +1,26 @@
-(function(ELEMENT) {
-    ELEMENT.matches = ELEMENT.matches || ELEMENT.mozMatchesSelector || ELEMENT.msMatchesSelector || ELEMENT.oMatchesSelector || ELEMENT.webkitMatchesSelector;
-    ELEMENT.closest = ELEMENT.closest || function closest(selector) {
-        if (!this) return null;
-        if (this.matches(selector)) return this;
-        if (!this.parentElement) {return null}
-        else return this.parentElement.closest(selector)
-      };
-}(Element.prototype));
+(function(e){
+  e.closest = e.closest || function(css){
+    var node = this;
 
-(function(e){ 
- e.closest = e.closest || function(css){ 
-   var node = this;
-  
-   while (node) { 
-      if (node.matches(css)) return node; 
-      else node = node.parentElement; 
-   } 
-   return null; 
- } 
+    while (node) {
+      if (node.matches(css)) return node;
+      else node = node.parentElement;
+    }
+    return null;
+  }
 })(Element.prototype);
+
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector ||
+    Element.prototype.webkitMatchesSelector ||
+    function(s) {
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+        i = matches.length;
+      while (--i >= 0 && matches.item(i) !== this) {}
+      return i > -1;
+    };
+}
