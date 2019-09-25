8dd948fede338398050f297545a89865df580d09
sienge <enge666-86@yandex.ru>
2019-09-25 23:55:12 +0300

    полифилл closest для ie11

--- Nikamed/js/polifill_closest.js
+++ Nikamed/js/polifill_closest.js
@@ -14,3 +14,14 @@ if (!Element.prototype.closest) {
     return null;
   };
 }
+
+;(function(e) {
+    var matches = e.matches || e.matchesSelector || e.webkitMatchesSelector || e.mozMatchesSelector || e.msMatchesSelector || e.oMatchesSelector;
+    !matches ? (e.matches = e.matchesSelector = function matches(selector) {
+        var matches = document.querySelectorAll(selector);
+        var th = this;
+        return Array.prototype.some.call(matches, function(e) {
+            return e === th;
+        });
+    }) : (e.matches = e.matchesSelector = matches);
+})(Element.prototype);
