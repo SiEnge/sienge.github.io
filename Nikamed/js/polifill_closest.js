5d750e6a236252bcb8cda3e911ed1c290e7d78fb
sienge <enge666-86@yandex.ru>
2019-09-25 23:39:44 +0300

    полифилл closest для ie11

--- Nikamed/js/polifill_closest.js
+++ Nikamed/js/polifill_closest.js
@@ -1,26 +1,16 @@
-(function(e){
-  e.closest = e.closest || function(css){
-    var node = this;
-
-    while (node) {
-      if (node.matches(css)) return node;
-      else node = node.parentElement;
-    }
-    return null;
-  }
-})(Element.prototype);
-
 if (!Element.prototype.matches) {
-  Element.prototype.matches =
-    Element.prototype.matchesSelector ||
-    Element.prototype.mozMatchesSelector ||
-    Element.prototype.msMatchesSelector ||
-    Element.prototype.oMatchesSelector ||
-    Element.prototype.webkitMatchesSelector ||
-    function(s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-    };
+  Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                              Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
 }
